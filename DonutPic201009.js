e4cba25c56cc4d92c31c5a93a99f72959b43adf7
KyungeunOh <47470474+KyungeunOh@users.noreply.github.com>
2020-10-13 21:46:08 +0900

    Update DonutPic201009.js

--- DonutPic201009.js
+++ DonutPic201009.js
@@ -21,33 +21,13 @@ function ProfileScreen({ navigation }) {
   );
 }
 
-
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    </View>
-  );
-}
-
-
-function SettingsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    </View>
-  );
-}
-
-
 const Stack = createStackNavigator();
-
 
 function MyStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Notifications" component={NotificationsScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
-      <Stack.Screen name="Settings" component={SettingsScreen} />
     </Stack.Navigator>
   );
 }
